7a61cb36cee651af724f9f602cb68b528ffb26ee
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-15 21:56:03 +0900

    미션 1 : 3강, 아주 간단한 형태의 잡인 HelloJob 을 만들어서 수행, h2 는 꼭 메모리 모드로

---
+++ src/main/java/com/ll/sbb20240111/batch/HelloJobConfig.java
@@ -0,0 +1,64 @@
+package com.ll.sbb20240111.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * 배치 작업을 설정하는 Spring Batch Job Configuration 클래스
+ * Job은 하나의 작업 단위를 나타낸다.
+ */
+/*
+미션 1
+세상에서 제일 간단한 배치 예제를 만들어서 실행해주세요.
+Hello World 라는 텍스트가 배치에 의해서 출력되어야 합니다.
+청크방식이 아닌 테스클릿 방식으로 만들어주세요.
+ */
+@Slf4j
+@Configuration
+public class HelloJobConfig {
+    // Job 생성 메서드
+    @Bean
+    public Job helloJob(JobRepository jobRepository, Step simpleStep1) { // simpleStep1는 helloStep1 이다. 잡이 한개인경우 분명해서 아무이름이나 붙여도 됨
+        // JobBuilder를 사용하여 'helloJob'이라는 이름의 Job을 생성하고, simpleStep1 스텝을 시작으로 설정
+        return new JobBuilder("helloJob", jobRepository)
+                .start(simpleStep1)
+                .build();
+    }
+
+    // Step 생성 메서드
+    @Bean
+    public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet1,
+                           PlatformTransactionManager platformTransactionManager) { // helloStep1Tasklet1는 helloStep1Tasklet1 이다.
+        // StepBuilder를 사용하여 'helloStep1Tasklet1'이라는 이름의 Step을 생성하고, Tasklet과 TransactionManager를 설정
+        return new StepBuilder("helloStep1Tasklet1", jobRepository)
+                .tasklet(helloStep1Tasklet1, platformTransactionManager)
+                .build();
+    }
+
+    // Tasklet 생성 메서드
+    @Bean
+    public Tasklet helloStep1Tasklet1() {
+        // Tasklet을 생성하고, execute 메서드에서 "Hello World"를 로깅하고 콘솔에 출력한 후 RepeatStatus.FINISHED를 반환
+        return ((contribution, chunkContext) -> {
+            log.info("Hello World");
+            System.out.println("Hello World");
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    /*
+    순서
+    helloStep1Tasklet1() 빈등록
+    -> helloStep1Tasklet1 객체가 연결되서 helloStep1()이 만들어지고 helloStep1 객체가 등록이 되서
+    -> helloJob이 실행된다.
+     */
+}
