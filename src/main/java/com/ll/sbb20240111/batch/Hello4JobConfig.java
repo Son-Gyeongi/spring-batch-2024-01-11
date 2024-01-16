167c51d85bb8cb18efd42ac587f8b43fc190fe0a
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-16 21:37:37 +0900

    18강, 청크방식으로 100이 출력될 때까지 난수 10개씩 추출하여 문자열로 바꾸고 10개씩 출력하는 hello4Job 배치잡 생성

--- src/main/java/com/ll/sbb20240111/batch/Hello4JobConfig.java
+++ src/main/java/com/ll/sbb20240111/batch/Hello4JobConfig.java
@@ -5,119 +5,104 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.builder.FlowJobBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 /**
  * 배치 작업을 설정하는 Spring Batch Job Configuration 클래스
  * Job은 하나의 작업 단위를 나타낸다.
+ *
+ *  Spring Batch를 사용하여 "hello4Job"이라는 배치 작업을 설정하는 클래스입니다.
+ *  "hello4Step1"은 청크 기반 작업으로, 원본 데이터를 읽어와 가공한 후 최종 결과를 화면에 출력
  */
 /*
 미션 5
 hello4Job 생성
     hello4Step1 : 태스클릿이 아닌 청크기반작업을 수행
+=> 17강, 청크방식으로 100이 출력될 때까지 난수 10개씩 추출하여 문자열로 바꾸고 10개씩 출력하는 hello4Job 배치잡 생성
  */
 @Slf4j
 @Configuration
 public class Hello4JobConfig {
-    // hello4Job을 설정하는 메서드
+    // "hello4Job" 배치 작업을 생성하는 메서드
     @Bean
-    public Job hello4Job(
-            JobRepository jobRepository,
-            Step hello4Step1,
-            Step hello4Step2,
-            Step hello4Step3
-    ) {
-        // 먼저 hello4Step1과 hello4Step2가 병렬로 실행되는 Flow를 생성
-        Flow flow1 = new FlowBuilder<SimpleFlow>("flow1")
+    public Job hello4Job(JobRepository jobRepository, Step hello4Step1) {
+        return new JobBuilder("hello4Job", jobRepository)
                 .start(hello4Step1)
-                .build();
-
-        Flow flow2 = new FlowBuilder<SimpleFlow>("flow2")
-                .start(hello4Step2)
-                .build();
-
-        // 병렬로 실행되는 두 개의 Flow(flow1, flow2)를 하나로 합치는 병렬 Flow 생성
-        Flow parallelFlow = new FlowBuilder<SimpleFlow>("parallelFlow")
-                .split(new SimpleAsyncTaskExecutor()) // 병렬 실행을 위한 실행기 설정
-                .add(flow1, flow2)
-                .build();
-
-        // 병렬 Flow(parallelFlow) 다음에 hello4Step3이 실행되는 Job 생성
-        FlowJobBuilder jobBuilder = new JobBuilder("hello4Job", jobRepository)
-                .start(parallelFlow)
-                .next(hello4Step3)
-                .end()
-                .incrementer(new RunIdIncrementer());
-
-        return jobBuilder.build();
-    }
-
-    // hello4Step1을 설정하는 메서드
-    @Bean
-    public Step hello4Step1(JobRepository jobRepository, ItemReader<String> hello4Step1Reader,
-                            ItemWriter<String> hello4Step1Writer, PlatformTransactionManager platformTransactionManager) {
-        // StepBuilder를 사용하여 'hello4Step1'이라는 이름의 Step을 생성하고, Chunk 기반의 Tasklet을 설정
-        return new StepBuilder("hello4Step1")
-                .<String, String>chunk(10) // 청크 기반으로 작업을 수행하며, 10개씩 처리
-                .reader(hello4Step1Reader)
-                .writer(hello4Step1Writer)
-                .transactionManager(platformTransactionManager)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    // hello4Step2를 설정하는 메서드
-    @Bean
-    public Step hello4Step2(JobRepository jobRepository, Tasklet hello4Step2Tasklet, PlatformTransactionManager platformTransactionManager) {
-        // StepBuilder를 사용하여 'hello4Step2Tasklet'이라는 이름의 Step을 생성하고, Tasklet과 TransactionManager를 설정
-        return new StepBuilder("hello4Step2Tasklet", jobRepository)
-                .tasklet(hello4Step2Tasklet, platformTransactionManager)
-                .build();
-    }
-
-    // hello4Step2의 Tasklet을 설정하는 메서드
-    @StepScope
-    @Bean
-    public Tasklet hello4Step2Tasklet() {
-        // Tasklet을 생성하고, execute 메서드에서 "Hello World 3-2"를 출력한 후 RepeatStatus.FINISHED를 반환
-        return ((contribution, chunkContext) -> {
-            System.out.println("Hello World 3-2");
-            return RepeatStatus.FINISHED;
-        });
-    }
-
-    // hello4Step3을 설정하는 메서드
+    // "hello4Step1" 청크 기반 작업을 설정하는 메서드
     @JobScope
     @Bean
-    public Step hello4Step3(JobRepository jobRepository, Tasklet hello4Step3Tasklet, PlatformTransactionManager platformTransactionManager) {
-        // StepBuilder를 사용하여 'hello4Step3Tasklet'이라는 이름의 Step을 생성하고, Tasklet과 TransactionManager를 설정
-        return new StepBuilder("hello4Step3Tasklet", jobRepository)
-                .tasklet(hello4Step3Tasklet, platformTransactionManager)
+    public Step hello4Step1(
+            JobRepository jobRepository,
+            Hello4Step1Reader hello4Step1Reader,
+            Hello4Step1Processor hello4Step1Processor,
+            Hello4Step1Writer hello4Step1Writer,
+            PlatformTransactionManager platformTransactionManager
+    ) {
+        return new StepBuilder("hello4Step1Tasklet", jobRepository)
+                .<Integer, String>chunk(10, platformTransactionManager)
+                .reader(hello4Step1Reader)
+                .processor(hello4Step1Processor)
+                .writer(hello4Step1Writer)
                 .build();
     }
 
-    // hello4Step3의 Tasklet을 설정하는 메서드
+    // 원본 데이터 읽기
+    // 원본 데이터를 읽어오는 ItemReader 구현 클래스
     @StepScope
-    @Bean
-    public Tasklet hello4Step3Tasklet() {
-        // Tasklet을 생성하고, execute 메서드에서 "Hello World 3-3"을 출력한 후 RepeatStatus.FINISHED를 반환
-        return ((contribution, chunkContext) -> {
-            System.out.println("Hello World 3-3");
-            return RepeatStatus.FINISHED;
-        });
+    @Component
+    public static class Hello4Step1Reader implements ItemReader<Integer> {
+        @Override
+        public Integer read() {
+            // 0에서 500 사이의 난수를 생성하여 반환
+            int no = (int) (Math.random() * 500);
+
+            // 특정 조건에 도달하면(null인 경우) 읽기를 중단
+            if (no==100) return null;
+
+            return no;
+        }
+    }
+
+    // 원본 데이터 가공해서 파생 데이터 생성
+    // 원본 데이터를 가공하여 새로운 형태로 변환하는 ItemProcessor 구현 클래스
+    // EX : 50 -> "no. 50"
+    @StepScope
+    @Component
+    public static class Hello4Step1Processor implements ItemProcessor<Integer, String> {
+        @Override
+        public String process(Integer item) {
+            // 원본 데이터를 가공하여 문자열 반환
+            return "no. " + item;
+        }
+    }
+
+    // 파생 데이터를 화면에 출력
+    // 최종 처리 결과를 화면에 출력하는 ItemWriter 구현 클래스
+    @StepScope
+    @Component
+    public static class Hello4Step1Writer implements ItemWriter<String> {
+        @Override
+        public void write(Chunk<? extends String> chunk) {
+            // 처리된 항목들을 화면에 출력
+            List<String> items = (List<String>) chunk.getItems();
+
+            for (String item : items) {
+                System.out.println("item = "+item);
+            }
+        }
     }
 }
